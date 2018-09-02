bc14852b5b379f7f90180edf87bcddb58843ee14
KAMALI GIS <42888383+kamaliGIS@users.noreply.github.com>
2018-09-02 18:38:13 +0800

    tes3
    bauwebgiss

---
+++ data/SaranaKesehatan_4.js
@@ -0,0 +1,11 @@
+var json_SaranaKesehatan_4 = {
+"type": "FeatureCollection",
+"name": "SaranaKesehatan_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAMOBJ": "RSUD Bau-bau", "FCODE": null, "LCODE": "GI0020", "REMARK": "Rumah Sakit Umum", "METADATA": null, "SRS_ID": null, "TIPRST": "0", "JPLYRS": "1", "ALAMAT": null }, "geometry": { "type": "Point", "coordinates": [ 122.591758236000032, -5.482998369999962, 0.0 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "RSU Murhum", "FCODE": null, "LCODE": "GI0020", "REMARK": "Rumah Sakit Umum", "METADATA": null, "SRS_ID": null, "TIPRST": "0", "JPLYRS": "1", "ALAMAT": null }, "geometry": { "type": "Point", "coordinates": [ 122.601954924000097, -5.462424971999951, 0.0 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Rumah Sakit Bhayangkara", "FCODE": null, "LCODE": "GI0020", "REMARK": "Rumah Sakit Umum", "METADATA": null, "SRS_ID": null, "TIPRST": "0", "JPLYRS": "1", "ALAMAT": null }, "geometry": { "type": "Point", "coordinates": [ 122.612952769000117, -5.458307052999942, 0.0 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Rumah Sakit Bhakti Medika", "FCODE": null, "LCODE": "GI0020", "REMARK": "Rumah Sakit Umum", "METADATA": null, "SRS_ID": null, "TIPRST": "0", "JPLYRS": "1", "ALAMAT": null }, "geometry": { "type": "Point", "coordinates": [ 122.608253138, -5.466753234999942, 0.0 ] } }
+]
+}
